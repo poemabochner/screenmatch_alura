0abba755acf883c8928946bc5c324364edce1ab4
Poema Bochner <poemabochner8@gmail.com>
2024-07-25 21:27:57 -0300

    criando conversor de dados

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosDoramas;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,5 +19,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=my+demon&apikey=1467ed36");
 		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosDoramas dados = conversor.obterDados(json, DadosDoramas.class);
+		System.out.println(dados);
 	}
 }
